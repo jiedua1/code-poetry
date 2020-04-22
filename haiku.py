9e6c5da1681328f31193c4476e0e5108813dfe56
jiedua1 <jiedongduan@gmail.com>
2020-04-22 12:18:38 -0500

    Added my python code poetry. I think it's kinda cool although kinda lame

---
+++ haiku.py
@@ -0,0 +1,18 @@
+# look, 5-7-5. 
+# 
+# haikus r EVERYWHERE here.
+# 
+# in the code, & BEYOND !!!!!!!!
+"""   |     |  """
+"""   |     |  """
+"""  \ /   \ / """
+"""   v     v  """
+this = " code makes a poem"
+haiku = " run it for a treat"
+print("\n(NOW) * (LOOK), %s * (BELOW) \
+\
+\
+\
+\
+\
+...\nu%s\nmy%s\n"%(this[13:], haiku, this))
